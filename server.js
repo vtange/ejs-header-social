48722e4dac860f7b60467c4a1980ece861babf7d
Victor Tang <victortang1992@gmail.com>
2016-03-07 18:48:32 -0800

    npm module conversion: server.js returns app, remove port, mongoDB, and main "/" route

--- server.js
+++ server.js
@@ -3,7 +3,6 @@
 // get all the tools we need
 var express  = require('express');
 var app      = express();
-var port     = process.env.PORT || 8080;
 var flash    = require('connect-flash');
 
 var morgan       = require('morgan');
@@ -12,10 +11,6 @@ var bodyParser   = require('body-parser');
 var methodOverride = require('method-override');
 var session      = require('express-session');
 // configuration ===============================================================
-var mongoose = require('mongoose');
-var configDB = require('./config/database.js');
-mongoose.connect(configDB.url); // connect to our database
-
 
 var passport = require('passport');
 require('./config/passport')(passport); // pass passport for configuration
@@ -29,6 +24,7 @@ app.use(bodyParser.json())    // parse application/json
 app.use(methodOverride());                  // simulate DELETE and PUT
 
 app.set('view engine', 'ejs'); // set up ejs for templating
+app.set('views', require('path').join(__dirname, 'views-bar'));
 
 // required for passport
 app.use(session({ secret: 'ilovescotchscotchyscotchscotch' })); // session secret
@@ -37,10 +33,9 @@ app.use(passport.session()); // persistent login sessions
 app.use(flash()); // use connect-flash for flash messages stored in session
 
 // routes ======================================================================
-require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
 require('./app/acct-manage/loginLogout.js')(app, passport); // load basic login logout routes
 require('./app/acct-manage/socialMedia.js')(app, passport); // load social media linking
 require('./app/acct-manage/passForget.js')(app); // load password forgot functionality
 // launch ======================================================================
-app.listen(port);
-console.log('The magic happens on port ' + port);
+
+module.exports = app;
