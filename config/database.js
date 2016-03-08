38d97c621d0ffcdaf64da591e5c5336d1865b695
Victor Tang <victortang1992@gmail.com>
2016-03-07 18:46:47 -0800

    npm module conversion: DB is connect at mother-app, not sub-app

--- config/database.js
+++
@@ -1,7 +0,0 @@
-console.log("	CONFIG/DATABASE.JS")
-// config/database.js
-module.exports = {
-
-    'url' :  process.env.MONGOLAB_URI || 'mongodb://localhost/polland' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
-
-};
