4ed3bdd472a175a5608f902b5f0a354caef71624
Victor Tang <victortang1992@gmail.com>
2016-04-25 22:31:03 -0700

    convert to social media addon

--- server.js
+++ server.js
@@ -5,16 +5,7 @@ module.exports = function(input_app,input_session,passport){
 var app      = input_app;
 var session = input_session;
 
-// set up statics
-var express  = require('express');
-app.use(express.static(__dirname + '/public'));     // set the static files location /public/img will be /img for users
-
-var flash    = require('connect-flash');
-app.use(flash()); // use connect-flash for flash messages stored in session
-
 // routes ======================================================================
-require('./app/acct-manage/loginLogout.js')(app, passport); // load basic login logout routes
 require('./app/acct-manage/socialMedia.js')(app, passport); // load social media linking
-require('./app/acct-manage/passForget.js')(app); // load password forgot functionality
 // no launch, this just connects stuff ======================================================================
 };
